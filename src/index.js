2dcfdc030942c76d310fde137d3b91a75fd5e8fb
Yasir Eymen Kayabaşı <yasirator04@gmail.com>
2023-11-11 15:03:25 +0300

    front-end kısmını yapmaya başladım

--- src/index.js
+++ src/index.js
@@ -9,10 +9,28 @@ app.get("/", function(req, res) {
 
 app.get("/:lang/:src", function(req, res) {
     if (req.params.lang === "public") {
-        res.sendFile(
-            __dirname + "/" +
-            req.params.src
-        )
+        switch (req.params.src) {
+            case "bootstrap-css":
+                res.sendFile(
+                    __dirname + "/css/bootstrap.min.css"
+                )
+                break;
+            
+            case "bootstrap-js":
+                res.sendFile(
+                    __dirname + "/js/bootstrap.bundle.min.js"
+                )
+                break;
+            
+            case "main-style-css":
+                res.sendFile(
+                    __dirname + "/css/style.css"
+                )
+                break;
+
+            default:
+                break;
+        }
     } else {
         res.sendFile(
             __dirname + "/" +
