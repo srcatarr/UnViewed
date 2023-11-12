7e92cda98de3cf7407389ece23cd7314026fd7bc
Yasir Eymen Kayabaşı <yasirator04@gmail.com>
2023-11-12 09:26:38 +0300

    sabah sabah front-end

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,11 @@ function getParam(name)  {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+document.querySelector("#launch").addEventListener("click", function() {
+    window.location.href += "/app";
+})
+
+document.querySelector("#download").addEventListener("click", function() {
+    window.location.href += "/download";
+})
