d472ec53e235200bcbdb31d215e8c5d97894eb69
Pstandsforproto <148652299+Pstandsforproto@users.noreply.github.com>
2023-11-25 18:32:50 +0000

    linuz desteği

--- server.sh
+++ server.sh
@@ -1,29 +1,92 @@
-# Server Commandline Interface for Linux
-
 #!/bin/bash
 
-if ["$1" == ""]; then
-    echo UnViewed ServerCI v0.0.0.1 Copyright C 2023 Yasir Eymen KAYABAŞI
-    echo Type "help" for more information
-    exit -b 0
+null=false
+
+if [ -z "$1" ]; then
+    null=true
 fi
 
-if ["$1" == "-install"]; then
-    if ["$2" == ""]; then
-        if [-e node_modules]; then
-            echo Node modules already installed
-            exit -b 0
+if [ "$null" == true ]; then
+    echo "UnViewed ServerCI v0.0.0.1 Copyright C 2023 Yasir Eymen KAYABASI"
+    echo "Type 'help' for more information"
+    exit 0
+fi
+
+if [ "$1" == "--run" ]; then
+    if [ -d "node_modules" ]; then
+        node src/index.js
+        exit 0
+    else
+        npm install
+        node src/index.js
+        exit 0
+    fi
+elif [ "$1" == "--install" ]; then
+    if [ -z "$2" ]; then
+        if [ -d "node_modules" ]; then
+            echo "Node modules already installed"
+            exit 0
         else
             npm install
-            exit -b 0
+            exit 0
         fi
     else
-        if [-e node_modules/$2]; then
-            echo This module already installed
-            exit -b 0
+        if [ -e "node_modules/$2" ]; then
+            echo "This module already installed"
+            exit 0
         else
-            npm add $2
-            exit -b 0
+            npm add "$2"
+            exit 0
         fi
     fi
-fi # Buralarda else kullan
+elif [ "$1" == "--update" ]; then
+    npm update
+    echo "Updated to the latest version"
+    exit 0
+elif [ "$1" == "--login" ]; then
+    if [ -z "$2" ]; then
+        echo "Please enter Google Apps Script Project ID"
+    else
+        echo "Trying login"
+        if [ -e ".env" ]; then
+            echo "You already logged in"
+            exit 0
+        else
+            echo "Logging in"
+            echo "# Google Apps Script Project ID" > .env
+            echo "GS_ID=$2" >> .env
+            echo "# Application PORT" >> .env
+            echo "PORT=3000" >> .env
+            exit 0
+        fi
+    fi
+elif [ "$1" == "--compile" ]; then
+    cd src/ts || exit
+    npx tsc
+    cd ../..
+elif [ "$1" == "--decompile" ]; then
+    cd src/ts || exit
+    rm -f db.js
+    echo "Codes decompiled"
+    cd ../..
+elif [ "$1" == "--version" ]; then
+    echo "UnViewed ServerCI Version 0.0.0.1"
+    exit 0
+elif [ "$1" == "--help" ]; then
+    echo "ServerCI: The Commandline Interface For UnViewed Server - Version 0.0.0.1"
+    echo ""
+    echo "COMMANDS"
+    echo ""
+    echo "    --help        More information about ServerCI."
+    echo "    --version     Learn version."
+    echo "    --install     Install packages."
+    echo "    --run         Run server in localhost."
+    echo "    --update      Update NodeJS and ServerCI."
+    echo "    --login       Login to Google Apps Script."
+    echo "    --compile     Compile codes to run."
+    echo "    --decompile   Decompile codes to run."
+else
+    echo "An error occurred: This command not found"
+fi
+
+exit 0
