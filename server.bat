a566d60d293cc0c9aa3d28e0449f7ab960b7f10b
Yasir Eymen Kayabaşı <yasirator04@gmail.com>
2023-11-25 11:55:38 +0300

    ServerCI programlıyorum

--- server.bat
+++ server.bat
@@ -1,24 +1,6 @@
 :: Server commandline interface for Windows
 @echo off
 
-echo ServerCI needs NodeJS 
-
-if "%1"=="" (
-    echo ServerCI v0.0.0.1
-    set /p cmd=
-    if %cmd%==run (
-        if exist node_modules (
-            node src/index.js
-        ) else (
-            npm install
-            node src/index.js
-        )
-    )
-    if %cmd%==install (
-        
-    )
-)
-
 if %1==run  (
     if exist node_modules (
         node src/index.js
@@ -46,4 +28,22 @@ if %1==install (
 
 if %1==update (
     npm update
+)
+
+if %1==version (
+    echo UnViewed ServerCI version 0.0.0.1
+)
+
+if "%1"=="" (
+    echo ServerCI v0.0.0.1
+    set /p cmd=
+    if %cmd%==run (
+        if exist node_modules (
+            node src/index.js
+        ) else (
+            npm install
+            node src/index.js
+        )
+    )
+    if %cmd%==install ()
 )
--- server.sh
+++ server.sh
@@ -6,6 +6,10 @@
 
 echo "ServerCI needs NodeJS"
 
+if ["$1" == ""]; then
+    # Utku'ya bırakıyorum
+fi
+
 if ["$1" == "run"]; then
     if [-d "node_modules"]; then
         node src/index.js
@@ -13,4 +17,19 @@ if ["$1" == "run"]; then
         npm install
         node src/index.js
     fi
+fi
+
+if ["$1" == "install"]; then
+    if ["$2" == ""]; then
+        if [-z "node_modules"]; then
+        fi
+    else
+        if [-z "node_modules/$2"]; then
+            echo This module already installed
+        else
+            npm add $2
+fi
+
+if ["$1" == "update"]; then
+    npm update
 fi
